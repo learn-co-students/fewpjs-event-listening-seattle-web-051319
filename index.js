f4dcc10db4fae347c3a9dd68a6d0a0d0619def68
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-06-25 08:32:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+
+  const input = document.getElementById('input');
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
+
+
 }
+// //
+// const input = document.getElementById('input');
+// input.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
